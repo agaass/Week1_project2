43e31314617941857cb87a3477857c30003b9dcb
Ali Gaass <ali.gaass@welltok.com>
2016-01-20 19:18:49 -0700

    answer to number 1

--- main.js
+++ main.js
@@ -8,8 +8,8 @@
 
 // 1. Declare a variable whose value is an empty Array. Use any method you choose to add at least 4 items to it.
 
-//console.log("Question 1");
-//console.log(yourAnswer);
+console.log("Question 1");
+console.log(yourAnswer);
 
 
 // 2. Add an additional item to the beginning of your Array.
